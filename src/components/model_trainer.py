7d1347cf930b13c57a4a7e9d1518c141409155b8
anshulgheewala <gheewalaanshul@gmail.com>
2025-08-03 16:38:06 +0530

    Implemented Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -35,13 +35,42 @@ class ModelTrainer:
 
             models = {
                 "Linear Regression": LinearRegression(),
-                "KNN": KNeighborsRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
                 "Random Forest": RandomForestRegressor(),
-                "AdaBoost": AdaBoostRegressor(),
+                "AdaBoost Regressor": AdaBoostRegressor(),
                 "XGBRegressor": XGBRegressor(),
-                "CatBoostRegressor": CatBoostRegressor(verbose=False),
-                "GradientBoosting": GradientBoostingRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "Gradient Boosting": GradientBoostingRegressor(),
+            }
+
+            params={
+                "Linear Regression":{},
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest":{
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
             }
 
             print(f"x_train shape: {x_train.shape}")
@@ -49,7 +78,7 @@ class ModelTrainer:
             print(f"x_test shape: {x_test.shape}")
             print(f"y_test shape: {y_test.shape}")
 
-            model_report: dict=evaluate_model(x_train = x_train, y_train = y_train, x_test = x_test, y_test = y_test, models=models)
+            model_report: dict=evaluate_model(x_train = x_train, y_train = y_train, x_test = x_test, y_test = y_test, models=models,params=params)
 
             best_model_score = max(sorted(model_report.values()))
 
@@ -61,6 +90,7 @@ class ModelTrainer:
                 raise CustomException("No best Model Found")
             
             logging.info(f"Best model found on both training and testing datasets is {best_model}")
+            logging.info(f"Best Params for the model is {best_model.get_params()}")
 
             save_object(file_path=self.model_trainer_config.trained_model_file_path, obj=best_model)
             
